4ef139365382c5fd722bdd0e76ab7339ccda556a
fkthat <fkthat@yandex.ru>
2023-10-21 08:21:23 +0300

    Aliases

---
+++ Modules/FkThat.PowerShell.Aliases/FkThat.PowerShell.Aliases.psm1
@@ -0,0 +1,3 @@
+Set-Alias code "$env:LOCALAPPDATA\Programs\Microsoft VS Code\bin\code.cmd"
+Set-Alias vim "$env:ProgramFiles\Vim\vim90\vim.exe"
+Set-Alias gvim "$env:ProgramFiles\Vim\vim90\gvim.exe"
