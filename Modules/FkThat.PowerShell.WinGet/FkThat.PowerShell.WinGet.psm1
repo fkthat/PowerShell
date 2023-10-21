d4f36ee2880e4fde53ece8fb6e26ddd7cb83ca09
fkthat <fkthat@yandex.ru>
2023-10-21 08:23:07 +0300

    WinGet

---
+++ Modules/FkThat.PowerShell.WinGet/FkThat.PowerShell.WinGet.psm1
@@ -0,0 +1,9 @@
+Set-Alias winget "$env:LocalAppData\Microsoft\WindowsApps\winget.exe"
+
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+    winget complete --word $wordToComplete --commandline $commandAst --position $cursorPosition |
+        ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
