46aac442bb801e1d0cb121fa1db09ba0f76f7b61
fkthat <fkthat@yandex.ru>
2023-10-21 07:51:55 +0300

    ReadLine

---
+++ Modules/FkThat.PowerShell.ReadLine/FkThat.PowerShell.ReadLine.psm1
@@ -0,0 +1,10 @@
+Set-PSReadLineOption -EditMode vi -ViModeIndicator Script `
+    -ViModeChangeHandler {
+        if ($args[0] -eq 'Command') {
+            # Set the cursor to a blinking block.
+            Write-Host -NoNewLine "`e[1 q"
+        } else {
+            # Set the cursor to a blinking line.
+            Write-Host -NoNewLine "`e[5 q"
+        }
+    }
