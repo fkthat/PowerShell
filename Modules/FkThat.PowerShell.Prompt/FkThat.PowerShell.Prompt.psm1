7b4f62ede9d45ce591570bd42b2a753d66479f3c
fkthat <fkthat@yandex.ru>
2023-10-21 06:49:27 +0300

    Prompt

---
+++ Modules/FkThat.PowerShell.Prompt/FkThat.PowerShell.Prompt.psm1
@@ -0,0 +1,36 @@
+function Prompt {
+    $h = [regex]::Escape($HOME)
+    $s = [regex]::Escape([System.IO.Path]::DirectorySeparatorChar)
+    $Path = (Get-Location).Path -replace "^$h(?<tail>$s.*)?",'~${tail}'
+    $Host.UI.RawUI.WindowTitle = $Path
+
+    $White = "`e[37m"
+    $Green = "`e[32m"
+    $Blue = "`e[34m"
+    $Red = "`e[31m"
+
+    $ResetCursor = $env:TERM_PROGRAM -ne "vscode" ? "`e[5 q" : ""
+
+    $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = New-Object Security.Principal.WindowsPrincipal $identity
+    $elevated = $principal.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+
+    if($elevated) {
+        $UserColor = $Red
+        $Suffix = "#"
+    } else {
+        $UserColor = $Green
+        $Suffix = '$'
+    }
+
+    $UserName = [System.Environment]::UserName
+    $MachineName = [System.Environment]::MachineName
+
+    "${White}PS " +
+    "$UserColor$UserName@$MachineName" +
+    "${White}:" +
+    "${Blue}$Path" +
+    "${White}$Suffix " +
+    "$ResetCursor"
+}
+
--- setup.ps1
+++ setup.ps1
@@ -1,1 +1,2 @@
-Copy-Item "$PSScriptRoot\Profile.ps1" $PROFILE
+Set-Content $PROFILE ('$env:PSModulePath = "' + "$PSScriptRoot\Modules" + ';$env:PSModulePath"')
+Add-Content $PROFILE 'Import-Module FkThat.PowerShell.Profile'
