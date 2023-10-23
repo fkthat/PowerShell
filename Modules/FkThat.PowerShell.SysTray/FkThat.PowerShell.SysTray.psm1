badfb6cd2c6db2e2e7e46b7b1f6f578cd6653d23
fkthat <fkthat@yandex.ru>
2023-10-23 12:28:39 +0300

    SysTray

--- Modules/FkThat.PowerShell.SysTray/FkThat.PowerShell.SysTray.psm1
+++ Modules/FkThat.PowerShell.SysTray/FkThat.PowerShell.SysTray.psm1
@@ -1,5 +1,11 @@
 function Reset-SysTray {
+    Stop-Process -Name 'explorer.exe' -Force
+
+    Remove-Item 'HKCU:\Control Panel\NotifyIconSettings' -Recurse -Force
+
     Remove-ItemProperty `
         'HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify\' `
         -Name 'IconStreams','PastIconsStream' -ErrorAction SilentlyContinue
+
+    Restart-Computer
 }
