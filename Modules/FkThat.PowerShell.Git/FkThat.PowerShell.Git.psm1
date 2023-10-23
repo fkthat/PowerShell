f35a36c0392460180e81b20745f7d1731f2936f4
fkthat <fkthat@yandex.ru>
2023-10-23 05:07:20 +0300

    Git

--- Modules/FkThat.PowerShell.Git/FkThat.PowerShell.Git.psm1
+++ Modules/FkThat.PowerShell.Git/FkThat.PowerShell.Git.psm1
@@ -1,1 +1,46 @@
-Set-Alias git "$env:ProgramFiles\Git\bin\git.exe"
+function Start-GitFlow {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, Position = 0)]
+        [string]
+        # The name of a new working branch.
+        $Name,
+
+        [Parameter()]
+        [string]
+        # The base branch for the flow.
+        $Base = (& $git branch --show-current)
+    )
+
+    & $git checkout $Base -b $Name &&
+    & $git fetch origin "${Base}:${Base}" &&
+    & $git rebase $Base &&
+    & $git push -u origin $Name
+}
+
+function Clear-GitRepo {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 0, ValueFromPipeline)]
+        [string[]]
+        # Path to the git repository directory.
+        $Path = ".",
+
+        [switch]
+        # Clean untracked files as well
+        $CleanUntracked
+    )
+
+    begin {
+        $x = $CleanUntracked ? "-x" : "-X"
+    }
+
+    process {
+        $RepoDir | ForEach-Object {
+            & $git clean -df $x -e '!.vs' -e '!*.suo' -e '!.vscode/*'
+        }
+    }
+}
+
+Set-Alias saflow Start-GitFlow
+Set-Alias clgit Clear-GitRepo
