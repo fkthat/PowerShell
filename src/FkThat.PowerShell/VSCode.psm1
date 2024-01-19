fc1d600c93437f36187cf4195d72b7481b45d6ea
fkthat <fkthat@yandex.ru>
2024-01-19 18:38:16 +0300

    #32: Update Start-VS and Start-VSCode

--- src/FkThat.PowerShell/VSCode.psm1
+++ src/FkThat.PowerShell/VSCode.psm1
@@ -2,18 +2,36 @@ $ErrorActionPreference = "Stop"
 
 $code = "$env:LOCALAPPDATA\Programs\Microsoft VS Code\bin\code.cmd"
 
+<#
+.SYNOPSIS
+Starts VS Code with a folder or file as an argument.
+#>
 function Start-VSCode {
     [CmdletBinding()]
     param (
         [Parameter(Position = 0, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [SupportsWildcards]
         [string[]]
+        # The path to the folder or file. Can be a wildcard.
         $Path = '.'
     )
 
+    begin {
+        $paths = @()
+    }
+
     process {
-        $Path | Get-Item | ForEach-Object {
-            cmd -ArgumentList @("/c", $code, "-n", $_)
-        }
+        $Path | Get-Item -ErrorAction SilentlyContinue |
+            ForEach-Object {
+                $paths += $_
+            }
+    }
+
+    end {
+        $paths | Select-Object -Unique |
+            ForEach-Object {
+                cmd -ArgumentList @("/c", $code, "-n", $_)
+            }
     }
 }
 
