becb8e498f6c7c6340e4bed526cb85864af8d866
fkthat <fkthat@yandex.ru>
2024-08-07 22:47:04 +0300

    save changes

--- src/FkThat.PowerShell.Profile/Environment.psm1
+++ src/FkThat.PowerShell.Profile/Environment.psm1
@@ -7,32 +7,86 @@ $EnvRegKey = @{
     Machine = 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment'
 }
 
+[Flags()]
+enum Scope {
+    User = 1
+    Machine = 2
+    All = 3
+}
+
+function _Get_EnvironmentVariable {
+    $EnvRegKey.Keys | ForEach-Object {
+            $scope = $_
+            $key = Get-Item $EnvRegKey[$scope]
+
+            $key.GetValueNames() | ForEach-Object {
+                [PSCustomObject]@{
+                    Scope = [Scope]$scope
+                    Name = $_
+                    Value = $key.GetValue($_, $null, 'DoNotExpandEnvironmentNames')
+                }
+            }
+        }
+}
+
+function _Get_UserEnvironmentVariable {
+
+}
+
+function _Get_MachineEnvironmentVariable {
+
+}
+
+function Get-UserEnvironmentVariable {
+
+}
+
+function Get-MachineEnvironmentVariable {
+
+}
+
 function Get-EnvironmentVariable {
     [CmdletBinding()]
     param (
-        [Parameter(Position = 0)]
+        [Parameter(Position = 0, ValueFromPipeline, ValueFromPipelineByPropertyName)]
         [SupportsWildcards()]
+        [ArgumentCompleter({
+            _Get_EnvironmentVariable_All |
+                Where-Object Name -Like "$($args[2])*" |
+                Select-Object -ExpandProperty Name
+        })]
         [string]
         $Name,
 
         [Parameter()]
-        [ValidateSet('User', 'Machine')]
-        [string]
-        $Scope
+        [Scope]
+        $Scope = 'All'
     )
 
-    # get all
-    $EnvRegKey.Keys | ForEach-Object {
-        $s = $_
-        $key = Get-Item $EnvRegKey[$s]
-        $key.GetValueNames() | ForEach-Object {
-            [PSCustomObject]@{
-                Scope = $s
-                Name = $_
-                Value = $key.GetValue($_, $null, 'DoNotExpandEnvironmentNames')
+    begin {
+        $allInScope = ForEach-Object {
+            $s = $_
+            $key = Get-Item $EnvRegKey[$s]
+            $key.GetValueNames() | ForEach-Object {
+                [PSCustomObject]@{
+                    Scope = $s
+                    Name = $_
+                    Value = $key.GetValue($_, $null, 'DoNotExpandEnvironmentNames')
+                }
             }
         }
-    } |
+
+        $filtered
+    }
+
+    process {
+        $Name | ForEach-Object {
+
+        }
+    }
+
+    # get all
+    $EnvRegKey.Keys | |
     # filter
     Where-Object {
         ((-not $Name) -or ($_.Name -like $Name)) -and
@@ -112,9 +166,9 @@ function Import-EnvironmentVariable {
     }
 }
 
-Set-Alias genv Get-Env
-Set-Alias senv Set-Env
-Set-Alias renv Remove-Env
-Set-Alias ipenv Import-Env
+Set-Alias genv Get-EnvironmentVariable
+Set-Alias senv Set-EnvironmentVariable
+Set-Alias renv Remove-EnvironmentVariable
+Set-Alias ipenv Import-EnvironmentVariable
 
 Export-ModuleMember -Function * -Alias *
